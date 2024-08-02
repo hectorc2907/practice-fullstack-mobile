d690c30c4bc02fdc47f6c0b57a819f7265c400cc
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-08-02 18:53:03 -0300

    probamos la propiedad Image

--- projects/Basic/App.js
+++ projects/Basic/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 const App = () => {
   return (
     <View style={style.container}>
-      <Text style={style.title}>Hello World!</Text>
+      <Text style={style.title}>Hello World!!</Text>
+      <Image
+        source={{
+          uri: "https://picsum.photos/200/200",
+        }}
+        style={style.image}
+      />
     </View>
   );
 };
@@ -17,6 +23,7 @@ const style = StyleSheet.create({
     backgroundColor: "#292929",
   },
   title: { fontSize: 30, color: "#fff" },
+  image: { height: 200, width: 200, borderRadius: 100 },
 });
 
 export default App;
