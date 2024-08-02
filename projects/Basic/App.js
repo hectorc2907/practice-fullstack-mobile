7f0607fca67572a6f3c52d883de1fe638005b386
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-08-02 18:42:16 -0300

    utilizamos la funcion de StyleSheet para el manejo de estilos

--- projects/Basic/App.js
+++ projects/Basic/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 30 }}>Hello World!</Text>
+    <View style={style.container}>
+      <Text style={style.title}>Hello World!</Text>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#292929",
+  },
+  title: { fontSize: 30, color: "#fff" },
+});
 
 export default App;
