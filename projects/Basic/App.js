aa93c14d7cc1735a6fa0ce8b255ceb253a49977c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-08-02 20:05:26 -0300

    se realiza la instalacion de expo-image-picker y probamos su funcionamiento para seleecionar imagenes

--- projects/Basic/App.js
+++ projects/Basic/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   View,
@@ -7,21 +7,41 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera is required");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+console.log(pickerResult)
+    if (pickerResult.canceled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.assets[0].uri });
+  };
+
   return (
     <View style={style.container}>
       <Text style={style.title}>Hello World!!</Text>
       <Image
         source={{
-          uri: "https://picsum.photos/200/200",
+          uri:
+            selectedImage !== null
+              ? selectedImage.localUri
+              : "https://picsum.photos/200/200",
         }}
         style={style.image}
       />
-      <TouchableOpacity
-        onPress={() => Alert.alert("Hello!!!")}
-        style={style.button}
-      >
+      <TouchableOpacity onPress={openImagePickerAsync} style={style.button}>
         <Text style={style.buttonText}>Press Me</Text>
       </TouchableOpacity>
     </View>
@@ -36,7 +56,7 @@ const style = StyleSheet.create({
     backgroundColor: "#292929",
   },
   title: { fontSize: 30, color: "#fff" },
-  image: { height: 200, width: 200, borderRadius: 100 },
+  image: { height: 200, width: 200, borderRadius: 100, resizeMode: "contain" },
   button: { backgroundColor: "deepskyblue", padding: 7, marginTop: 10 },
   buttonText: { color: "#fff", fontSize: 20 },
 });
