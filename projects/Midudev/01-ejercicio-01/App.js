3fbdf64395d0e25136967410369288042f05adbe
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-08-08 21:00:01 -0300

    probamos las propiedades de imagenes

--- projects/Midudev/01-ejercicio-01/App.js
+++ projects/Midudev/01-ejercicio-01/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{
+          uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg",
+        }}
+        style={{ width: 215, height: 294 }}
+      />
       <StatusBar style="auto" />
+      <Text style={{ color: "white" }}>Tenemos aqui la app!</Text>
     </View>
   );
 }
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
