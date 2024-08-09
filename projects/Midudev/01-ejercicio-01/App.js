48b0495e0806d967d4dbf736dcf6e812899b857c
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-08-08 22:48:27 -0300

    configuramos los view para las pantalla de cualquier dispositivo

--- projects/Midudev/01-ejercicio-01/App.js
+++ projects/Midudev/01-ejercicio-01/App.js
@@ -1,30 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
-import { getLatestGames } from "./lib/metacritic";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Main } from "./components/Main";
 
 export default function App() {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    getLatestGames().then((games) => {
-      setGames(games);
-    });
-  }, []);
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <ScrollView>
-        {games.map((game) => (
-          <View key={game.slug} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
-            <Text style={styles.title}>{game.title}</Text>
-            <Text style={styles.score}>{game.score}</Text>
-            <Text style={styles.description}>{game.description}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -34,23 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
-  },
-  card: { marginBottom: 42 },
-  image: { width: 107, height: 147, borderRadius: 10 },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 10,
-    color: "#fff",
-  },
-  description: {
-    fontSize: 16,
-    color: "#fff",
-  },
-  score: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: "10",
+    paddingHorizontal: 12,
   },
 });
